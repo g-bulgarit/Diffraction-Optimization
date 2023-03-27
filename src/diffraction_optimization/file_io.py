81acdaed74ba9d4234ff9e7224b1595262f892ec
Alon Budker <alonio6@gmail.com>
2023-03-27 22:50:35 +0300

    refactor: format with black

--- src/diffraction_optimization/file_io.py
+++ src/diffraction_optimization/file_io.py
@@ -8,10 +8,12 @@ def load_dataset_csv_to_df(filepath: str) -> pd.DataFrame:
 
 
 def get_vectors_of_specific_digit(dataset: pd.DataFrame, digit: int) -> pd.DataFrame:
-    return dataset[dataset['label'] == digit]
+    return dataset[dataset["label"] == digit]
 
 
-def load_digit_images_from_dataset(dataset: pd.DataFrame, digit: int, load_amt=None) -> np.ndarray:
+def load_digit_images_from_dataset(
+    dataset: pd.DataFrame, digit: int, load_amt=None
+) -> np.ndarray:
     digit_vectors = get_vectors_of_specific_digit(dataset, digit)
     if load_amt is None:
         vectors_amt = len(digit_vectors)
@@ -19,5 +21,7 @@ def load_digit_images_from_dataset(dataset: pd.DataFrame, digit: int, load_amt=N
         vectors_amt = load_amt
     digit_matrix = np.zeros((28, 28, vectors_amt))
     for index in range(vectors_amt):
-        digit_matrix[:, :, index] = parse_mnist_digit_to_matrix(digit_vectors.iloc[index])
+        digit_matrix[:, :, index] = parse_mnist_digit_to_matrix(
+            digit_vectors.iloc[index]
+        )
     return digit_matrix
