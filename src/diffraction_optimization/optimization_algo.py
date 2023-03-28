f6b9d4a0a5bff09c5228c8411ebadd65ee74cb0a
Alon Budker <alonio6@gmail.com>
2023-03-28 21:17:58 +0300

    add: evaluate model on test set

--- src/diffraction_optimization/optimization_algo.py
+++ src/diffraction_optimization/optimization_algo.py
@@ -2,8 +2,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from diffraction_optimization.diffraction_model import DiffractionSystem
 from diffraction_optimization.image_proccessing import (
-    generate_horizontal_line,
-    generate_random_phase_mask,
+    # generate_horizontal_line,
+    # generate_random_phase_mask,
     mutate_phase_mask,
 )
 from diffraction_optimization.file_io import (
@@ -99,10 +99,33 @@ def simulated_annealing_optimization():
     axes[0].plot(one_predicition_rates, label="One: True Positive Rate")
     axes[0].plot(average_prediction_rates, label="Average True Positive Rate")
     axes[0].legend()
-    axes[0].set_title(f"Predication rate summary ({num_iterations} iterations)")
+    axes[0].set_xlabel("Iteration [#]")
+    axes[0].set_ylabel("Correct Prediction [%]")
 
     axes[1].imshow(phase_mask)
     axes[1].set_title("Final Phase Mask")
+    plt.show()
+    pass
+
+    # Now evaluate on new data:
+    zero_digits_test_set = load_digit_images_from_dataset(
+        mnist_dataset, 0, load_amt=200
+    )[:, :, 51:]
+    one_digits_test_set = load_digit_images_from_dataset(
+        mnist_dataset, 1, load_amt=200
+    )[:, :, 51:]
+
+    (
+        zero_true_positive_rate,
+        one_true_positive_rate,
+    ) = system.generate_predictions(
+        zero_images=zero_digits_test_set, one_images=one_digits_test_set
+    )
+
+    plt.figure()
+    plt.title("Classification Results on Test Set")
+    plt.bar("Zero True Positive Rate", zero_true_positive_rate)
+    plt.bar("One True Positive Rate", one_true_positive_rate)
     plt.show()
     pass
 
